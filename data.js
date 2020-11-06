77f41a8b30e863ddd0ba3a1647dd6cf4205e5bc3
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2020-11-06 13:17:19 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,73 @@
+var jsonData = [
+  {
+    "q" : "html means?",
+    "opt1": "Hyper text markup langage",
+    "opt2" : "hyper type marrpup language",
+    "opt3" : "none of these",
+    "answer" : "Hyper text markup language"
+  },
+  {
+    "q" : "css means",
+    "opt1": "cascading style sheet",
+    "opt2" : "cascade style sheet",
+    "opt3" : "none of the above",
+    "answer" : "cascading style sheet"
+  },
+  {
+    "q" : "which is header tag",
+    "opt1": "head tag",
+    "opt2" : "body tag",
+    "opt3" : "none of these",
+    "answer" : "head tag"
+  },
+  {
+    "q" : "which is breakline tag",
+    "opt1": "break tag",
+    "opt2" : "body tag",
+    "opt3" : "none of these",
+    "answer" : "break tag"
+  },
+  {
+    "q" : "which is span tag",
+    "opt1": "span",
+    "opt2" : "style",
+    "opt3" : "none of these",
+    "answer" : "span tag"
+  },
+  {
+    "q" : "which is section tag",
+    "opt1": "div tag",
+    "opt2" : "style tag",
+    "opt3" : "none of these",
+    "answer" : "div tag"
+  },
+  {
+    "q" : "which is style tag",
+    "opt1": "style tag",
+    "opt2" : "select tag",
+    "opt3" : "none of these",
+    "answer" : "style tag"
+  },
+  {
+    "q" : "which is selection tag",
+    "opt1": "select tag",
+    "opt2" : "head tag",
+    "opt3" : "none of these",
+    "answer" : "select tag"
+  },
+  {
+    "q" : "which is paragraph tag",
+    "opt1": "header tag",
+    "opt2" : "paragraph tag",
+    "opt3" : "none of these ",
+    "answer" : "paragragh tag"
+  },
+  {
+    "q" : "which is heading tag",
+    "opt1": "head tag",
+    "opt2" : "paragragh tag",
+    "opt3" : "none of these",
+    "answer" : "head tag"
+  },
+  
+];
