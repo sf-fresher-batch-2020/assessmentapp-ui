21a05cdd29a5c8868d86e8c4a484dd33b72c126e
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2020-11-13 09:28:22 +0530

    Add files via upload

---
+++ loginservice.js
@@ -0,0 +1,21 @@
+class LoginService {
+    login = function(email, password, role){
+        var usersTemp = JSON.parse(localStorage.getItem("USERS"));
+        var users= usersTemp ? usersTemp: [];     
+        let exists = false;   
+        for(let obj of users)
+        {
+            if( obj.email == email  && obj.password == password && obj.user == role)
+            {
+                exists=true;
+                var users=[];
+                var usersTemp = JSON.parse(localStorage.getItem("LOGGED_IN_USER"));
+                var users= usersTemp ? usersTemp:[];
+                localStorage.setItem("LOGGED_IN_USER", JSON.stringify(obj ) );
+                localStorage.getItem("LOGGED_IN_USER");
+                break;
+            }
+        }
+        return exists;
+    }
+}
